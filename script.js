c44edb13c76c3705c2b03a8f096063d6899827b8
Nazar <bodaknazar@gmail.com>
2018-07-10 23:55:47 +0300

    to many item

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-	fetch('https://jsonplaceholder.typicode.com/posts/1')
+	fetch('https://jsonplaceholder.typicode.com/posts')
   	.then(response => response.json())
  	.then(json => {
- 		$('#demoH').html(json.id + '. ' + json.title);
- 	 	$('#demoP').html(json.body)
+ 		for(var i = 0; i < json.length; i++){
+ 			$('#demo').append('<h3>' + json[i].id + '. ' +
+ 			 json[i].title + '.</h3><p>' + json[i].body + '.</p>');
+ 		}
  	});
 });
